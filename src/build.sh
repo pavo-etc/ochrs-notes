ce9192d4f60f46c81aa6766a5279977e6b489dc9
Zach Manson <zachpmanson@gmail.com>
2022-08-20 11:19:57 +0800

    Add src

---
+++ src/build.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Lint
+clang-format -i main.c
+
+# Cleanup
+rm -f ./nchrs
+rm ../site/*.html
+rm ../site/links/*
+
+cp -R links/ ../site/links/
+
+# Linux(debug)
+#cc -std=c89 -DDEBUG -Wall -Wno-unknown-pragmas -Wpedantic -Wshadow -Wuninitialized -Wextra -Werror=implicit-int -Werror=incompatible-pointer-types -Werror=int-conversion -Wvla -g -Og -O0 -fsanitize=address -fsanitize=undefined main.c -o nchrs
+
+# Linux(fast)
+cc main.c -std=c89 -Os -DNDEBUG -g0 -Wall -Wno-comment -Wno-unknown-pragmas -o nchrs
+
+# RPi
+# tcc -Wall main.c -o oscean
+
+# Plan9
+# pcc main.c -o oscean
+
+# Valgrind
+# gcc -std=c89 -DDEBUG -Wall -Wpedantic -Wshadow -Wuninitialized -Wextra -Werror=implicit-int -Werror=incompatible-pointer-types -Werror=int-conversion -Wvla -g -Og main.c -o oscean
+# valgrind ./oscean
+
+# Build Size
+# echo "$(du -b ./nchrs | cut -f1) bytes written"
+ echo "$(du ./nchrs | cut -f1) bytes written"
+
+# Run
+./nchrs
+
+# Cleanup
+rm -f ./nchrs
---
+++ src/deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+./batchVariants
+./build.sh
+echo "Uploading to webserver..."
+rsync -rvhP --inplace  --include=".htaccess" --exclude={'.*','src'} $HOME/projects/c/nchrs/ uberspace:html/
